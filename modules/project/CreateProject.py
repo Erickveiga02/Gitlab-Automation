3d3da74659477417c4c159d0bc5cf6cbe3ee1d85
Erick Veiga <erickveigaf2009@gmail.com>
2022-11-14 20:20:07 -0300

    Update CreateProject.py

--- modules/project/CreateProject.py
+++ modules/project/CreateProject.py
@@ -11,8 +11,8 @@ class ProjectCreate:
 
     def create_project(self):
         try:
-            os.environ['GROUP_ID'] = '4'
-            os.environ['PROJECT_NAME'] = 'Createproject-erick'
+            os.environ['GROUP_ID'] 
+            os.environ['PROJECT_NAME'] 
             group = os.getenv('GROUP_ID')
             project_name = os.getenv('PROJECT_NAME')
             project = gl.projects.create({'name': project_name, 'namespace_id': group})

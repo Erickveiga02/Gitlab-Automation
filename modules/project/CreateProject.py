8cf4688f3d6f062e40820124fc613c254d3b915d
Erick da Silva Veiga <erick.veiga.ext@sascar.com.br>
2022-11-15 11:37:14 -0300

    update

--- modules/project/CreateProject.py
+++ modules/project/CreateProject.py
@@ -1,16 +1,24 @@
 import gitlab
 import os
-
+import argparse
 from ..config.ConnectGitlab import gl
+
+
 
 class ProjectCreate:
 
     def create_project(self):
         try:
-            os.environ['GROUP_ID'] 
-            os.environ['PROJECT_NAME'] 
-            group = os.getenv('GROUP_ID')
-            project_name = os.getenv('PROJECT_NAME')
+            my_parser = argparse.ArgumentParser(description='Projeto tem como objetivo gerenciar o Gitlab através de um CLI')
+            my_parser.add_argument('-p',
+                                   metavar='--project',
+                                   type=str,
+                                   help='Nome do projeto que deseja criar')
+
+            my_parser.add_argument('-g', metavar='--group', type=int, help='ID do groupo que deseja criar o projeto')
+            args = my_parser.parse_args()
+            project_name = args.p
+            group = args.g
             project = gl.projects.create({'name': project_name, 'namespace_id': group})
             print("Project created")
 

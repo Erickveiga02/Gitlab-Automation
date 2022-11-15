e2ff9aa582bb6a2d25ebcefc75f78f06e34aea87
Erick da Silva Veiga <erick.veiga.ext@sascar.com.br>
2022-11-15 09:53:04 -0300

    dir: new dir config and gitlab connect

--- modules/project/CreateProject.py
+++ modules/project/CreateProject.py
@@ -1,18 +1,13 @@
 import gitlab
 import os
-os.environ['GITLAB_URL'] = 'http://192.168.15.200'
-gitlab_url = os.getenv('GITLAB_URL')
-os.environ['GITLAB_TOKEN'] = 'glpat-Z7wV3vKk1_WRvnmCWY-z'
-gitlab_token = os.getenv('GITLAB_TOKEN')
-gl = gitlab.Gitlab(url=gitlab_url, private_token=gitlab_token,keep_base_url=True)
-gl.auth()
-gl.enable_debug()
+from ..config.ConnectGitlab import gl
+
 class ProjectCreate:
 
     def create_project(self):
         try:
             os.environ['GROUP_ID'] = '4'
-            os.environ['PROJECT_NAME'] = 'Createproject-erick'
+            os.environ['PROJECT_NAME'] = 'Createasdaadasdproject-er2ick'
             group = os.getenv('GROUP_ID')
             project_name = os.getenv('PROJECT_NAME')
             project = gl.projects.create({'name': project_name, 'namespace_id': group})
@@ -20,8 +15,6 @@ class ProjectCreate:
 
         except gitlab.exceptions.GitlabCreateError as e:
             print("Project already exists")
-
-
 
 
 

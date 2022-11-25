75f456ebe893aa383e85be2d88c8170405d0a17c
Erick Veiga <erickveigaf2009@gmail.com>
2022-11-25 14:45:39 -0300

    Update ConnectGitlab.py

--- modules/config/ConnectGitlab.py
+++ modules/config/ConnectGitlab.py
@@ -1,9 +1,9 @@
 import os
 import gitlab
 
-os.environ['GITLAB_URL'] = 'http://192.168.15.200'
+os.environ['GITLAB_URL'] 
 gitlab_url = os.getenv('GITLAB_URL')
-os.environ['GITLAB_TOKEN'] = 'glpat-Z7wV3vKk1_WRvnmCWY-z'
+os.environ['GITLAB_TOKEN']
 gitlab_token = os.getenv('GITLAB_TOKEN')
 gl = gitlab.Gitlab(url=gitlab_url, private_token=gitlab_token,keep_base_url=True)
 gl.auth()
